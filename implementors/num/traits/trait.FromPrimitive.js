33e126bb764d2f2157c6ac78e95c6c4753726310
Sam Doshi <sam@metal-fish.co.uk>
2015-04-25 09:37:08 +0000

    Update documentation

---
+++ implementors/num/traits/trait.FromPrimitive.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['num'] = [];implementors['midi'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='num/traits/trait.FromPrimitive.html' title='num::traits::FromPrimitive'>FromPrimitive</a> for <a class='enum' href='midi/enum.Channel.html' title='midi::Channel'>Channel</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
